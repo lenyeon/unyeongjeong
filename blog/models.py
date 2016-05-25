b53ff45929c0cc778ed2e40fc498a4927284a1ea
lenyeon <lenyeon@gmail.com>
2016-05-25 14:59:10 +0900

    make html

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
+# Create your models here.
